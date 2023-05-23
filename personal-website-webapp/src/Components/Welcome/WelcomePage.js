56935478fc275aed40fb56335d30a23d46e5d0cf
Thomas Chin <thomas081904@gmail.com>
2023-05-23 00:33:19 -0500

    Cleaned Up Code - Added Initial Welcome Page Rough Outline

---
+++ personal-website-webapp/src/Components/Welcome/WelcomePage.js
@@ -0,0 +1,19 @@
+import {React} from "react";
+
+function Welcome() {
+    return (
+        <section className = "welcome-section">
+            <div className = "description">
+                <h1>
+                    Welcome! 
+                </h1>
+                <h2>
+                    Hi, I'm Thomas Chin, Computer Science student at Georgia Institute of Technology 
+                    interested in Software Engineering, System Infastructure, and Data Analysis
+                </h2>
+            </div>
+        </section>
+    );
+}
+
+export default Welcome;
