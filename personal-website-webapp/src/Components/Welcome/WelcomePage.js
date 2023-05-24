5004a3d2f4efb839974068639e03f91dffbdc4df
Thomas Chin <thomas081904@gmail.com>
2023-05-23 23:29:47 -0500

    Worked on Welcome page
    Added image, and added typing effect, still need to fix formatting

--- personal-website-webapp/src/Components/Welcome/WelcomePage.js
+++ personal-website-webapp/src/Components/Welcome/WelcomePage.js
@@ -1,19 +1,47 @@
-import {React} from "react";
+import { React } from "react";
+import { TypeAnimation } from "react-type-animation";
+import {Stack, Box} from '@mui/material';
+import personalPhoto from "./personal-photo.png";
+
+import "./Welcome.scss";
 
 function Welcome() {
-    return (
-        <section className = "welcome-section">
-            <div className = "description">
-                <h1>
-                    Welcome! 
-                </h1>
-                <h2>
-                    Hi, I'm Thomas Chin, Computer Science student at Georgia Institute of Technology 
-                    interested in Software Engineering, System Infastructure, and Data Analysis
-                </h2>
-            </div>
-        </section>
-    );
+  return (
+    <section className="welcome-section">
+        <Stack direction="row" className="description">
+          <Box className="b1">
+            <h5>
+              <TypeAnimation
+                sequence={["Welcome!", 1000]}
+                wrapper="div"
+                cursor={false}
+                repeat={0}
+              />
+            </h5>
+            <h2>&nbsp;</h2>
+            <h5>
+              <TypeAnimation
+                sequence={["\n ", 1500, "I'm Thomas Chin"]}
+                wrapper="div"
+                cursor={false}
+                repeat={0}
+              />
+            </h5>
+
+              <p>
+                I'm a Computer Science Student at Georgia Institute of
+                Technology
+                {/* Software Engineer, Infastructure Engineer,
+                and Data Analyst  */}
+              </p>
+
+          </Box>
+          <Box className="personal-photo">
+            <img id="personal-photo" src={personalPhoto} alt="personal" />
+          </Box>
+        </Stack>
+    </section>
+  );
 }
 
 export default Welcome;
