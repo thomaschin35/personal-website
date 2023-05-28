df9efc8c30f98becd5608354e94e07459921c0d5
Thomas Chin <thomas081904@gmail.com>
2023-05-28 00:11:31 -0500

    Made Changes to About me scss

--- personal-website-webapp/src/Components/AboutMe/AboutMe.js
+++ personal-website-webapp/src/Components/AboutMe/AboutMe.js
@@ -1,43 +1,63 @@
-import {React} from 'react';
-import {Box} from "@mui/material";
-import './AboutMe.scss';
-
+import { React } from "react";
+import { Box, Stack } from "@mui/material";
+import "./AboutMe.scss";
 
 function AboutMe() {
-    const PersonalFact = (props) => {
-        const { title, body } = props;
-        return (
-            <div className="personal-facts-item"> 
-                <h5>{title}</h5>
-                <p>{body}</p>
-            </div>
-        );
-    }
-
-    return(
-        <section className="about-me">
-            <Box className="personal-facts">
-                <Box className="firstCol">
-                    <PersonalFact title="Education" body={
-                        <>
-                            B.S Computer Science | Intelligence & Information Internetworks
-                            <br/>
-                            Georgia Institute of Technology | GPA 4.0/4.0 | Faculty Honors
-                        </>
-                    }/>
-                </Box>
-                <Box className="SecondCol">
-                    <PersonalFact title="Education" body={
-                        <>
-                            B.S Computer Science | Intelligence & Information Internetworks
-                            <br/>
-                            Georgia Institute of Technology | GPA 4.0/4.0 | Faculty Honors
-                        </>
-                    }/>
-                </Box>
-
-            </Box>
-        </section>
+  const PersonalFact = (props) => {
+    const { title, body } = props;
+    return (
+      <div className="personal-facts-item">
+        <p className="title">{title}</p>
+        <p className="body">{body}</p>
+      </div>
     );
+  };
+
+  return (
+    <section className="about-me">
+      <div className="personal-facts">
+        <h5 className="header">ABOUT ME!</h5>
+        <Stack direction="row" className="hstack"> 
+          <Box className="col">
+            <PersonalFact
+              title="Education"
+              body={
+                <>
+                  B.S Computer Science | Intelligence & Information
+                  Internetworks
+                  <br />
+                  Georgia Institute of Technology | GPA 4.0/4.0 | Faculty Honors
+                </>
+              }
+            />
+            <PersonalFact
+              title="Education"
+              body={
+                <>
+                  B.S Computer Science | Intelligence & Information
+                  Internetworks
+                  <br />
+                  Georgia Institute of Technology | GPA 4.0/4.0 | Faculty Honors
+                </>
+              }
+            />
+          </Box>
+          <Box className="col">
+            <PersonalFact
+              title="Education"
+              body={
+                <>
+                  B.S Computer Science | Intelligence & Information
+                  Internetworks
+                  <br />
+                  Georgia Institute of Technology | GPA 4.0/4.0 | Faculty Honors
+                </>
+              }
+            />
+          </Box>
+        </Stack>
+      </div>
+    </section>
+  );
 }
 export default AboutMe;
